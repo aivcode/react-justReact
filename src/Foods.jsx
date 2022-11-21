cd0ed08607b081fe55fe8cb368a6e1b43d9a6d24
aiv.code <aiv.email@yandex.ru>
2022-11-21 11:19:51 +0300

    Add images and Food component

---
+++ src/Foods.jsx
@@ -0,0 +1,49 @@
+import React, {Fragment, useState} from "react";
+import "./Foods.css";
+
+
+const Foods = (props) => {
+  return (
+    /* 
+      Fragments in React allow us to group items without having to wrap them
+      inside another element, such as a div. Here, in our case, there is no
+      need to add a new div. Instead, we can use a fragment. It is important to
+      note that fragments do not add extra DOM nodes, whereas a plain <div>
+      naturally does. If we want to add styles to the container or if it serves
+      some other purpose, we can use a div element as a container. 
+    */
+    <Fragment>
+      <h4 className="foodTitle">Choose from our Menu</h4>
+      <ul className="ulFoods">
+        {
+          props.foodItems.map((item) => {
+            return (
+              /*
+                Keys help React identify which items have changed, are added,
+                or are removed. Keys should be given to the elements inside
+                the array to give the elements a stable identity. The best way
+                to pick a key is to use a string that uniquely identifies a
+                list item among its siblings. Most often you would use IDs
+                from your data as keys.
+              */
+              <li className="liFoods" key="item.id">
+                <img
+                  className="foodImg"
+                  src={require(`./images/${item.image}`)}
+                  alt={item.name}
+                />
+                <div className="foodItem">
+                  <p className="foodDesc">{item.desc}</p>
+                  <p className="foodPrice">{item.price}$</p>
+                </div>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </Fragment>
+  );
+};
+
+
+export default Foods;
