6a68061d05dadc930a11dc5c2cc2ea9faf39302d
aiv.code <aiv.email@yandex.ru>
2022-11-21 23:14:30 +0300

    FoodOrder comp started

--- src/FoodOrder.jsx
+++ src/FoodOrder.jsx
@@ -2,4 +2,16 @@ import React, {Fragment, useState} from "react";
 import "./FoodOrder.css";
 
 
+const FoodOrder = (props) => {
+  const selectedFood = props.food;
+  const [quantity, setQuantity] = useState(1);
+  const [totalAmount, setTotalAmount] = useState(selectedFood.price);
+  const [isOrdered, setIsOrdered] = useState(false);
 
+  const handleQuantityChange = (event) => {
+    setTotalAmount(selectedFood.price * event.target.value);
+    setQuantity(event.target.value);
+  };
+
+  // handleClick pg. 173
+}
