bf064b9505656692f276af7cdd011413ca92aaba
aiv.code <aiv.email@yandex.ru>
2022-11-23 12:35:18 +0300

    Handling errors with ErrorFunctionalBoundary comp

--- src/FoodOrder.jsx
+++ src/FoodOrder.jsx
@@ -1,5 +1,6 @@
 import React, {Fragment, useContext, useState} from "react";
 import { foodItemsContext } from "./App";
+import ErrorFunctionalBoundary from "./ErrorFunctionalBoundary";
 import "./FoodOrder.css";
 
 
@@ -8,11 +9,17 @@ const FoodOrder = (props) => {
   const [quantity, setQuantity] = useState(1);
   const [totalAmount, setTotalAmount] = useState(selectedFood.price);
   const [isOrdered, setIsOrdered] = useState(false);
+  const [isErrorCatched, setIsErrorCatched] = useState(false);
+
   const menuItems = useContext(foodItemsContext);
 
   const handleQuantityChange = (event) => {
-    setTotalAmount(selectedFood.price * event.target.value);
-    setQuantity(event.target.value);
+    try {
+      setTotalAmount(selectedFood.price * event.target.value);
+      setQuantity(event.target.value);
+    } catch {
+      setIsErrorCatched(true);
+    }
   };
 
   const handleClick = () => {
@@ -26,67 +33,72 @@ const FoodOrder = (props) => {
 
   return (
     <>
-      <h4 className="selFoodTitle">{selectedFood.name}</h4>
-      <img
-        src={require(`./images/${selectedFood.image}`)}
-        alt={selectedFood.name}
-        className="selFoodImg"
-      />
-      <ul className="ulFoodDetails">
-        <li>
-          <p className="selFoodDesc">{selectedFood.desc}</p>
-        </li>
-        <li>
-          <p className="selFoodPrice">{totalAmount}$</p>
-        </li>
-        <li className="selQuantity">
-          <label>Quantity</label>
-          <input
-            type="nubmer"
-            defaultValue={1}
-            className="quantity"
-            min="1"
-            max="10"
-            onChange={handleQuantityChange}
+      {!isErrorCatched && (
+        <>
+          <h4 className="selFoodTitle">{selectedFood.name}</h4>
+          <img
+            src={require(`./images/${selectedFood.image}`)}
+            alt={selectedFood.name}
+            className="selFoodImg"
           />
-        </li>
-        <li className="liDetails">
-          <label for="name"></label>
-          <input
-            type="text"
-            className="inputFields"
-            name="name"
-            id="name"
-            placeholder="Your Name"
-          />
-        </li>
-        <li>
-          <label for="mobile"></label>
-          <input
-            type="text"
-            className="inputFields"
-            id="mobile"
-            name="mobile"
-            placeholder="Your mobile number"
-          />
-        </li>
+          <ul className="ulFoodDetails">
+            <li>
+              <p className="selFoodDesc">{selectedFood.desc}</p>
+            </li>
+            <li>
+              <p className="selFoodPrice">{totalAmount}$</p>
+            </li>
+            <li className="selQuantity">
+              <label>Quantity</label>
+              <input
+                type="nubmer"
+                defaultValue={1}
+                className="quantity"
+                min="1"
+                max="10"
+                onChange={handleQuantityChange()}
+              />
+            </li>
+            <li className="liDetails">
+              <label for="name"></label>
+              <input
+                type="text"
+                className="inputFields"
+                name="name"
+                id="name"
+                placeholder="Your Name"
+              />
+            </li>
+            <li>
+              <label for="mobile"></label>
+              <input
+                type="text"
+                className="inputFields"
+                id="mobile"
+                name="mobile"
+                placeholder="Your mobile number"
+              />
+            </li>
 
-        <li>
-          <button className="btn btnOrder" onClick={handleClick}>
-            Submit Order
-          </button>
-          <button className="btn btnReturnMenu" onClick={props.returnToMenu}>
-            Return to Menu
-          </button>
-        </li>
-        {isOrdered && (
-          <li className="liMessage">
-            <label>
-              Order Submitted! You will receive an SMS to once ready for pickup.
-            </label>
-          </li>
-        )}
-      </ul>
+            <li>
+              <button className="btn btnOrder" onClick={handleClick}>
+                Submit Order
+              </button>
+              <button className="btn btnReturnMenu" onClick={props.returnToMenu}>
+                Return to Menu
+              </button>
+            </li>
+            {isOrdered && (
+              <li className="liMessage">
+                <label>
+                  Order Submitted! You will receive an SMS to once ready for pickup.
+                </label>
+              </li>
+            )}
+          </ul>
+        </>
+      )}
+      {isErrorCatched && <ErrorFunctionalBoundary />}
     </>
   );
 };
