278dac614d23d78fa23f5bd31b63092b526bcb98
aiv.code <aiv.email@yandex.ru>
2022-11-28 17:47:08 +0300

    Redesign with Hooks and func comps

--- src/Time/App.jsx
+++ src/Time/App.jsx
@@ -1,28 +1,52 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
 import Time from "./Time";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: true
-    };
-  }
 
-  render() {
-    return (
-      <div>
-        {this.state.open && (
-          <>
-            <Time />
-            <button onClick={() => this.setState({ open: false })}>
-              Close Time
-            </button>
-          </>
-        )}
-        {!this.state.open && <h1>Time Display Closed</h1>}
-      </div>
-    );
-  }
-}
+const App = (props) => {
+  /*
+     The useState Hook updated the variable’s state,
+     which caused the component to re-render. 
+  */
+  const [open, setOpen] = useState(true);
+  const closeTime = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      {open && (
+        <>
+          <Time />
+          <button onClick={closeTime}>Close Time</button>
+        </>
+      )}
+      {!open && <h1>Time display closed</h1>}
+    </div>
+  );
+};
+export default App;
+
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       open: true
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.state.open && (
+//           <>
+//             <Time />
+//             <button onClick={() => this.setState({ open: false })}>
+//               Close Time
+//             </button>
+//           </>
+//         )}
+//         {!this.state.open && <h1>Time Display Closed</h1>}
+//       </div>
+//     );
+//   }
+// }
--- src/Time/Time.jsx
+++ src/Time/Time.jsx
@@ -1,33 +1,45 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 
-class Time extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentTime: new Date().toLocaleString(),
-      close: false,
-    };
-  }
-  componentDidUpdate(prevState) {
-    if (prevState.currentTime !== this.state.currentTime) {
-      console.log("Time is changing.");
-    }
-  }
-  componentWillUnmount() {
-    console.log(`The Time component is going to be unmounted at ${this.state.currentTime}`);
-  }
-  componentDidMount() {
-    console.log(`The Time component is mounted at ${this.state.currentTime}`);
+function Time(props) {
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
+  useEffect(() => {
+    console.log("I am useEffect and I got invoked");
     setInterval(() => {
-      this.setState({
-        currentTime: new Date().toLocaleString()
-      });
+      console.log("Time changing");
+      setCurrentTime(new Date().toLocaleString());
     }, 1000);
-  }
-  render() {
-    return <h1>{this.state.currentTime}</h1>;
-  }
+  }, []);
+  return <h1>{currentTime}</h1>;
 }
 
 export default Time;
+
+// class Time extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       currentTime: new Date().toLocaleString(),
+//       close: false,
+//     };
+//   }
+//   componentDidUpdate(prevState) {
+//     if (prevState.currentTime !== this.state.currentTime) {
+//       console.log("Time is changing.");
+//     }
+//   }
+//   componentWillUnmount() {
+//     console.log(`The Time component is going to be unmounted at ${this.state.currentTime}`);
+//   }
+//   componentDidMount() {
+//     console.log(`The Time component is mounted at ${this.state.currentTime}`);
+//     setInterval(() => {
+//       this.setState({
+//         currentTime: new Date().toLocaleString()
+//       });
+//     }, 1000);
+//   }
+//   render() {
+//     return <h1>{this.state.currentTime}</h1>;
+//   }
+// }
