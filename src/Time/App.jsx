3eb13a84ea106c6b3b9b4981f3516e3463c8e11e
aiv.code <aiv.email@yandex.ru>
2022-11-28 12:52:49 +0300

    Life Of a Class example

---
+++ src/Time/App.jsx
@@ -0,0 +1,28 @@
+import "./styles.css";
+import React from "react";
+import Time from "./Time";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.open && (
+          <>
+            <Time />
+            <button onClick={() => this.setState({ open: false })}>
+              Close Time
+            </button>
+          </>
+        )}
+        {!this.state.open && <h1>Time Display Closed</h1>}
+      </div>
+    );
+  }
+}
