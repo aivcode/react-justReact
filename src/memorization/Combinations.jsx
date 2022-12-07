5d61c8545489d528042a53bb6901d1919bcb7b0e
aiv.code <aiv.email@yandex.ru>
2022-12-07 12:10:42 +0300

    React.memo example

--- src/memorization/Combinations.jsx
+++ src/memorization/Combinations.jsx
@@ -1,10 +1,17 @@
-const Combinations = ({ countBooks }) => {
+import React from "react";
+
+/*
+  The Combinations component is now wrapped in React.memo. React.memo memorizes
+  the whole component, so it only re-renders when the props change, in this 
+  case countBooks. 
+*/
+const Combinations = React.memo(({ countBooks }) => {
   console.log("Combinations component is re-rendered");
   let arrangements = 1;
-  for (let i = 2; i < countBooks; i++) {
+  for (let i = 2; i <= countBooks; i++) {
     arrangements *= i;
   }
   return `The total number of ways you can arrange the books is : ${arrangements}`;
-};
+});
 
 export default Combinations;
