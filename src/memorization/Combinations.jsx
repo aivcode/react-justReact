67be1f8324a9ddb997a864ea442d14fd7d3842a9
aiv.code <aiv.email@yandex.ru>
2022-12-08 14:47:00 +0300

    Custom hook example

--- src/memorization/Combinations.jsx
+++ src/memorization/Combinations.jsx
@@ -1,4 +1,5 @@
-import React, { useMemo } from "react";
+import React from "react";
+import useFactorial from "./Hooks/useFactorial";
 
 /*
   The Combinations component is now wrapped in React.memo. React.memo memorizes
@@ -9,19 +10,12 @@ const Combinations = React.memo(({ countBooks, checkSpace }) => {
   console.log("Combinations component is re-rendered");
   let space = "";
   switch (countBooks) {
-    case 1 - 5:
-      space = "Free space available";
-      break;
-    case 5 - 10:
-      space = "Perfect";
-      break;
-    case 10 - 15:
-      space = "Need extra storage";
-      break;
-    // default:
-    //   space = "Not sufficient";
+    case 1 - 5: space = "Free space available"; break;
+    case 5 - 10: space = "Perfect"; break;
+    case 10 - 15: space = "Need extra storage"; break;
+    default: space = "Not sufficient";
   }
-
+  console.log(space);
   /*
     The useMemo() returns the memorized output if there is no change in the
     dependency [countBooks]. So, when the shelfName changes, it returns the
@@ -29,19 +23,13 @@ const Combinations = React.memo(({ countBooks, checkSpace }) => {
     output and returns it. In this way, we can improve performance by
     memorizing expensive code executions with the useMemo() Hook.
   */
-  let arrangements = useMemo(() => {
-    console.log("Total number of ways being calculated");
-    let arrs = 1;
-
-    for (let i = 2; i <= countBooks; i++) {
-      arrs *= i;
-    }
-    return arrs;
-  }, [countBooks]);
 
   return (
     <>
-      <p>The total number of ways you can arrange books is {arrangements}</p>
+      <p>
+        The total number of ways you can arrange books is{" "}
+        {useFactorial(countBooks)}
+      </p>
       <button onClick={() => checkSpace(space)}>Check Space</button>
     </>
   );
