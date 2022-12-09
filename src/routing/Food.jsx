89091e55dbaf9423f670075ef2011b727b6bf476
aiv.code <aiv.email@yandex.ru>
2022-12-09 17:21:43 +0300

    Routing example

---
+++ src/routing/Food.jsx
@@ -0,0 +1,7 @@
+export default function Food() {
+  return (
+    <>
+      <h1 style={{ color: "blue" }}>You are on the order page</h1>
+    </>
+  );
+}
---
+++ src/routing/Menu.jsx
@@ -0,0 +1,8 @@
+export default function Menu() {
+  return (
+    <>
+      <h1 style={{ color: "green" }}>You are on the Home Page, Menu</h1>
+      <ul></ul>
+    </>
+  );
+}
